8ca5179211397175ee23cfacf2b34540f68bb353
cherif59 <cherif59282@gmail.com>
2026-04-09 12:35:54 +0200

    add  delay before setting default sink

--- audiorelay-virtual-sink.sh
+++ audiorelay-virtual-sink.sh
@@ -8,6 +8,7 @@ if ! pactl list short sinks | grep -q "VirtualSink"; then
     pactl load-module module-null-sink sink_name=VirtualSink sink_properties=device.description="VirtualSink"
 fi
 
+sleep 0.1
 pactl set-default-sink VirtualSink
 
 /opt/audiorelay/bin/AudioRelay --audio-output VirtualSink.monitor # Change path if necessary
